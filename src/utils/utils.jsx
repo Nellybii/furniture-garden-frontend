481b0fbd013500794c2d406eea27f6cf33014ada
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-03-01 11:06:57 +0300

    changes

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -3,7 +3,7 @@ import axios from "axios";
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:5000`
-    : process.env.BASE_URL;
+    : `https://furniture-project-18.onrender.com/`
 
 
 export const api = axios.create({
