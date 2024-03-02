4f233917ab847f46b6a780393971f6f4203bdfd3
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-03-02 09:06:50 +0300

    render url

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -3,11 +3,11 @@ import axios from "axios";
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:5000`
-    : `https://furniture-project-18.onrender.com/`
+    : `https://furniture-project-23.onrender.com/`
 
 
 export const api = axios.create({
-      baseURL: BASE_URL,
+      baseURL: `https://furniture-project-19.onrender.com`,
       responseType: "json",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`, 
