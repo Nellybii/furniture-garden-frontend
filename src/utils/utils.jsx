8555fa8614ebefe6d15334fd303ce4633b81e35b
Nelly Chepkemoi <nellychepkem2015@gmail.com>
2024-03-20 17:54:22 +0300

    changes

--- src/utils/utils.jsx
+++ src/utils/utils.jsx
@@ -3,10 +3,10 @@ import axios from "axios";
 export const BASE_URL =
   process.env.NODE_ENV === "development"
     ? `http://localhost:5000`
-    : `https://furniture-project25.onrender.com`;
+    : `https://furniture-project-25.onrender.com/`;
 
     export const api = axios.create({
-      baseURL: `https://furniture-project25.onrender.com`,
+      baseURL: `https://furniture-project-25.onrender.com/`,
       responseType: "json",
       headers: {
         Authorization: `Bearer ${localStorage.getItem("token")}`,
